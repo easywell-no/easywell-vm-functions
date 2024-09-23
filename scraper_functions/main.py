afdccc242bd7ba19d9a615f66c074a74709dfbbd
easywell-no <hello@easywell.no>
2024-09-23 15:21:24 +0000

    fix

--- scraper_functions/main.py
+++ scraper_functions/main.py
@@ -1,21 +1,21 @@
 import os
 from dotenv import load_dotenv
-from supabase import create_client, Client
 
 print("Hello World")
 
 # Load environment variables
-load_dotenv(dotenv_path='./.env')
+load_dotenv(dotenv_path='./scraper_functions/.env')
 
-# Access Supabase credentials from environment variables
+# Print all environment variables to check if they were loaded
+for key, value in os.environ.items():
+    if "SUPABASE" in key:
+        print(f"{key}: {value}")
+
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 
-# Print the loaded values to verify
 print(f"Loaded SUPABASE_URL: {SUPABASE_URL}")
 print(f"Loaded SUPABASE_KEY: {SUPABASE_KEY[:5]}...")
 
 if SUPABASE_URL is None or SUPABASE_KEY is None:
     raise Exception("Environment variables not loaded properly.")
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
