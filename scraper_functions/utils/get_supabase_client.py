00ecbe15f826eb920336fc1715421cbbeeabc837
easywell-no <hello@easywell.no>
2024-10-11 18:44:14 +0000

    added

--- scraper_functions/utils/get_supabase_client.py
+++ scraper_functions/utils/get_supabase_client.py
@@ -5,6 +5,8 @@ from supabase import create_client, Client
 def get_supabase_client() -> Client:
     SUPABASE_URL = os.getenv('SUPABASE_URL')
     SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+    SUPABASE_SERVICE_ROLE_KEY = os.getenv('SUPABASE_SERVICE_ROLE_KEY')
+
 
     if not SUPABASE_URL or not SUPABASE_KEY:
         error_msg = "Environment variables SUPABASE_URL or SUPABASE_KEY not set."
